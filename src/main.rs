116ffb40012310393777ae6a56e8ceb1e6478dc1
Gabriel Borges <gabriel.carvalho@terramagna.com.br>
2022-06-08 11:20:30 -0300

    remove binary from repo

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
-use std::io::{self, Write};
+use std::{io::{self, Write}, cmp::Ordering};
 use rand::Rng;
 
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(0..101);
+    let secret_number: u32 = rand::thread_rng().gen_range(0..101);
 
     println!("let's guess the number!");
-    print!("guess it: ");
-    io::stdout().flush().unwrap();
-
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read =(");
+    loop {   
+        print!("guess it: ");
+        io::stdout().flush().unwrap();
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read =(");
+        
+        let guess: u32 = guess.trim().parse().expect("please enter a integer");
     
-    println!("you guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("ᵗᵒᵒ ᵐᵘᶜʰ"),
+            Ordering::Less => println!("𝗠  𝗢  𝗥  𝗘 !"),
+            Ordering::Equal => {
+                println!("you won, congratulation :D");
+                break;
+            },
+        }
+    }
     println!("the secret number was {}", secret_number);
 }

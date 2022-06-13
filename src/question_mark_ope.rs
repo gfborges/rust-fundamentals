022784428e1518bce4fd35b80952b1c3fee9d102
Gabriel Borges <gabriel.carvalho@terramagna.com.br>
2022-06-13 12:11:24 -0300

    feat: question mark operator

---
+++ src/question_mark_ope.rs
@@ -0,0 +1,17 @@
+use std::fs::File;
+use std::io::{self, Read};
+
+fn main() {
+    match read_file("~/hello.txt") {
+        Ok(content) => println!("{}", content),
+        Err(_) => println!("Bad file name =("),
+    }
+}
+
+fn read_file(file_name: &str) -> Result<String, io::Error> {
+    let mut file = File::open(file_name)?;
+    let mut s = String::new();
+    file.read_to_string(&mut s)?;
+    Ok(s)
+
+}
